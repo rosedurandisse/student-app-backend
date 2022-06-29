61b086eeab9fd08bab3f637c1e357187534b719a
Rose Durandisse <rosedurandisse@pursuit.org>
2022-06-29 13:18:29 -0400

    Added the students controllers, with the starting routes, as well as the placeholder data

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const express = require("express");
 //initialize app
 const app = express();
 
+const studentsController = require("./controllers/studentsController");
+
+app.use("/students", studentsController);
+
 //route
 app.get("/", (request, response) => {
   response.send("Hello world");
---
+++ controllers/studentsController.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const controller = express.Router();
+
+const studentData = require("../studentData.json");
+
+controller.get("/", (request, response) => {
+  response.json(studentData);
+});
+
+controller.get("/:id", (request, response) => {
+  try {
+    if (!/[0-9]/.test(request.params.id)) {
+      response.send("Student id must be a number");
+      //why is the return necessary, getting a cannot set headers after they are sent to the client
+      return;
+    }
+
+    const selectedStudent = studentData.students.find(
+      (eachStudent) => eachStudent.id === request.params.id
+    );
+
+    if (selectedStudent) {
+      response.json(selectedStudent);
+    } else {
+      response.send("Student not found");
+    }
+  } catch (err) {
+    response.status(500).send("An error occured");
+  }
+});
+
+module.exports = controller;
