1a954f705df7bacbcc1584f8f68cc9cd9ed9ae74
Rose Durandisse <rosedurandisse@pursuit.org>
2022-07-01 00:17:31 -0400

    Added cors

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 //import code for express
 const express = require("express");
+const cors = require("cors");
 
 //initialize app
 const app = express();
 
 const studentsController = require("./controllers/studentsController");
 const namesController = require("./controllers/namesController");
+
+app.use(cors());
 
 app.use("/students", studentsController);
 app.use("/name", namesController);
